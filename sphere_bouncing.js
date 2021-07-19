7222c6c128dc44d87852dd68ef0edf41270eeb2c
5064 <5og4.kirin@gmail.com>
2021-07-19 17:54:35 +0900

    wip

--- sphere_bouncing.js
+++ sphere_bouncing.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
+const readline = require("readline")
+
 const SCREEN_WIDTH = 50
 const SCREEN_HEIGHT = 50
 const R = 5  // radius
 // distance between screen to object center z
-const K2 = R
+let K2 = R
 // K1 is distance between eye to screen z
-const K1 = 3 / 5 * SCREEN_WIDTH;
+const K1 = 2 / 5 * SCREEN_WIDTH;
+// 仮想領域の奥行きの最大値 球3つ分とする
+const SPACE_Z = R * 2 * 3
+const TEXTURE = " .-~:!=+*o$@";
 
-const TEXTURE = " .-~:=+*o$@";
+const INIT_V_THETA = -Math.PI/6  // 移動ベクトルのx軸方向の角度
+const INIT_V_PHI = Math.PI/4  // 移動ベクトルのy軸方向の角度
+
+const vector = {x:Math.cos(INIT_V_THETA),y:Math.sin(INIT_V_PHI), z: SPACE_Z/100}  // 移動単位ベクトル
+const v = {x:0,y:0,z:K2}
+
+const SPEED = 2  // 移動単位ベクトルの係数
 
 function sphereSurface() {
     result = []
@@ -16,15 +27,21 @@ function sphereSurface() {
             // 3D polar coordinates
             let x = R * Math.sin(theta) * Math.cos(phi)
             let y = R * Math.sin(theta) * Math.sin(phi)
-            let z = R * Math.cos(theta) + K2
-            result.push({ x, y, z })
+            let z = R * Math.cos(theta)
+            result.push({ x:(x), y:(y), z: (z + v.z)})
         }
     }
     return result
 }
 
 function convertTo2d(xyz) {
-    return { x: (K1 * xyz.x / (xyz.z + K2)), y: (K1 * xyz.y / (xyz.z + K2)), z: xyz.z }
+    return { x: (K1 * xyz.x / (xyz.z + v.z))+v.x, y: (K1 * xyz.y / (xyz.z + v.z))+v.y, z: xyz.z }
+}
+
+function move() {
+  v.x += vector.x * SPEED
+  v.y += vector.y * SPEED
+  v.z += vector.z * SPEED
 }
 
 function show(xyzs) {
@@ -45,18 +62,24 @@ function show(xyzs) {
         }
     }
     let output = ""
-    const maxZ = K2 + R
     for (const p of projection) {
         for (const z of p) {
             // luminance range is 0 to texture length 
             // Texture.length / (maxZ / z)
-            const i = Math.floor((TEXTURE.length - 1) / (maxZ / z))
+            const i = Math.floor((TEXTURE.length - 1) / (SPACE_Z / z))
 
             output += TEXTURE[i] + " "
         }
         output += "|\n"
     }
-    console.log(output)
+    return output
 }
 
-show(sphereSurface())
+setInterval(function() {
+    const output = show(sphereSurface())
+    readline.cursorTo(process.stdout, 0, 0);
+    process.stdout.cursorTo(0);
+    process.stdout.write("\x1B[?25l")  // clear cursor
+    process.stdout.write(output);
+    move()
+}, 500);
